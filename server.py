4d67ee82f8ae0e5684bf1cf692bd04fde4a24817
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-12-20 21:11:28 -0700

    finished assignment

--- server.py
+++ server.py
@@ -1,1 +1,22 @@
-test 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def userTable():
+    users = [
+        {'first_name' : 'Michael', 'last_name' : 'Choi'},
+        {'first_name' : 'John', 'last_name' : 'Supsupin'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+    usersLength = len(users)
+    return render_template("index.html", users = users, usersLength = usersLength)
+
+# no page found
+@app.errorhandler(404)
+def pageNotFound(missing):
+    return "Sorry! No response. Try again."
+
+if __name__=="__main__":
+    app.run(debug=True)
